daa70f9dfa9d745c7d6880bc62a6912bd27f8978
TjarkVanGuhlo <95100407+TjarkVanGuhlo@users.noreply.github.com>
2025-12-06 14:18:37 +0100

    refactor(deps): remove unused django-storages and S3 utilities
    - Delete frontend_server/utils.py (S3 storage backends)
    - Remove django-storages from dependencies
    - Remove storages from INSTALLED_APPS in settings

--- environment/frontend_server/frontend_server/utils.py
+++
@@ -1,6 +0,0 @@
-from storages.backends.s3boto import S3BotoStorage
-
-def StaticRootS3BotoStorage():
-    return S3BotoStorage(location="static")
-def MediaRootS3BotoStorage():
-    return S3BotoStorage(location="media")
